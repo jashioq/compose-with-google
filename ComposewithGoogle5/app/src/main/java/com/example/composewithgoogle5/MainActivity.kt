a9031f9475b0053d993df0ebfed456598f6aba29
Jan Wylegała <jajuwa@Jans-Mac-mini.local>
2023-08-22 17:09:34 +0200

    Finished compose quadrant app project

--- ComposewithGoogle5/app/src/main/java/com/example/composewithgoogle5/MainActivity.kt
+++ ComposewithGoogle5/app/src/main/java/com/example/composewithgoogle5/MainActivity.kt
@@ -3,28 +3,25 @@ package com.example.composewithgoogle5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement.Start
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextAlign.Companion.Start
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composewithgoogle5.ui.theme.ComposeWithGoogle5Theme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +33,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    ComposeQuadrant(
+                        firstTitle = "Text composable",
+                        secondTitle = "Image composable",
+                        thirdTitle = "Row composable",
+                        fourthTitle = "Column composable",
+                        firstText = "Displays text and follows the recommended Material Design guidelines.",
+                        secondText = "Creates a composable that lays out and draws a given Painter class object.",
+                        thirdText = "A layout composable that places its children in a horizontal sequence.",
+                        fourthText = "A layout composable that places its children in a vertical sequence.",
+                    )
                 }
             }
         }
@@ -46,32 +53,113 @@ class MainActivity : ComponentActivity() {
 fun ComposeQuadrant(firstTitle: String, secondTitle: String, thirdTitle: String,
                     fourthTitle: String, firstText: String, secondText: String, thirdText: String,
                     fourthText: String, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-//            .fillMaxSize()
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        Column {
-            Text(
-                text = "1",
-                textAlign = TextAlign.Start,
-//                modifier = Modifier
-//                    .width(50.dp)
-//                    .align(alignment = Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "2"
-            )
-        }
-        Column {
-            Row {
-                Text(
-                    text = "."
-                )
+        Row (
+            modifier = Modifier.weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFEADDFF))
+                    .padding(16.dp)
+            ) {
+                Column (
+                    modifier = Modifier
+                        .align(alignment = Alignment.Center)
+                ) {
+                    Text(
+                        text = firstTitle,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                    )
+                    Text(
+                        text = firstText,
+                        textAlign = TextAlign.Justify,
+                    )
+                }
             }
-            Row {
-                Text(
-                    text = "."
-                )
+            Box (
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFD0BCFF))
+                    .padding(16.dp)
+            ) {
+                Column (
+                    modifier = Modifier
+                        .align(alignment = Alignment.Center)
+                ) {
+                    Text(
+                        text = secondTitle,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                    )
+                    Text(
+                        text = secondText,
+                        textAlign = TextAlign.Justify,
+                    )
+                }
+            }
+        }
+        Row (
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Box (
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFB69DF8))
+                    .padding(16.dp)
+            ) {
+                Column (
+                    modifier = Modifier
+                        .align(alignment = Alignment.Center)
+                ) {
+                    Text(
+                        text = thirdTitle,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                    )
+                    Text(
+                        text = thirdText,
+                        textAlign = TextAlign.Justify,
+                    )
+                }
+            }
+            Box (
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(Color(0xFFF6EDFF))
+                    .padding(16.dp)
+            ) {
+                Column (
+                    modifier = Modifier
+                        .align(alignment = Alignment.Center)
+                ) {
+                    Text(
+                        text = fourthTitle,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                    )
+                    Text(
+                        text = fourthText,
+                        textAlign = TextAlign.Justify,
+                    )
+                }
             }
         }
     }
